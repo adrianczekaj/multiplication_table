30e208f84957b77fe4ba59cc903d7515e4dabc71
adrianczekaj <aczekaj.inf@gmail.com>
2022-07-25 22:43:01 +0200

    cleaned up code and added header

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header>
+            <h1>
+                <span style={{color: 'darkyellow'}}>M</span>
+                <span style={{color: 'lightcoral'}}>N</span>
+                <span style={{color: 'darkgoldenrod'}}>O</span>
+                <span style={{color: 'lightslategray'}}>Ż</span>
+                <span style={{color: 'darkorange'}}>E</span>
+                <span style={{color: 'lightseagreen'}}>N</span>
+                <span style={{color: 'darkorchid'}}>I</span>
+                <span style={{color: 'lightsalmon'}}>E</span>
+            </h1>
+        </header>
+    );
+};
+
+export default Header;
